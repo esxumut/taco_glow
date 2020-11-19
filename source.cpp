19af23d3728577f7fb0fd9fab9aae2686ab937fd
Esx_UmuT <73911906+esxumut@users.noreply.github.com>
2020-11-19 22:24:46 +0300

    Update source.cpp
    compiled as x86. Run the project as an administrator.

--- source.cpp
+++ source.cpp
@@ -12,20 +12,28 @@ HANDLE hProcess;
 bool toggle = true;
 
 
-uintptr_t GetmodulebaseAddress(const char* modName) { 
+uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName)
+{
+	uintptr_t modBaseAddr = 0;
 	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId);
-	if (hSnap != INVALID_HANDLE_VALUE) {
+	if (hSnap != INVALID_HANDLE_VALUE)
+	{
 		MODULEENTRY32 modEntry;
 		modEntry.dwSize = sizeof(modEntry);
-		if (Module32First(hSnap, &modEntry)) {
-			do {
-				if (!strcmp(modEntry.szModule, modName)) {
-					CloseHandle(hSnap);
-					return (uintptr_t)modEntry.modBaseAddr;
+		if (Module32First(hSnap, &modEntry))
+		{
+			do
+			{
+				if (!_wcsicmp(modEntry.szModule, modName))
+				{
+					modBaseAddr = (uintptr_t)modEntry.modBaseAddr;
+					break;
 				}
 			} while (Module32Next(hSnap, &modEntry));
 		}
 	}
+	CloseHandle(hSnap);
+	return modBaseAddr;
 }
 
 
@@ -170,7 +178,7 @@ int main() {
 	csgowindow = FindWindowA(NULL, "Counter-Strike: Global Offensive");
 	GetWindowThreadProcessId(csgowindow, &procId);
 	hProcess = OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE, NULL, procId);
-	modulebaseaddr = GetmodulebaseAddress("client.dll");
+	uintptr_t modulebaseaddr = GetModuleBaseAddress(procId, L"client.dll");
 
 
 
